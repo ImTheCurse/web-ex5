09693eb44087e95803bdd852d8fae542934d1443
ImTheCurse <33231841+ImTheCurse@users.noreply.github.com>
2024-07-15 01:41:22 +0300

    Bug Fix: Fixed access code bug.

--- controllers/prefController.js
+++ controllers/prefController.js
@@ -8,12 +8,14 @@ exports.prefController = {
 
         const { dbConnection } = require('../db_connection');
         const connection = await dbConnection.createConnection();
-
-        connection.execute(`insert into tbl_59_users(access_code,user_name,user_password) values('${uuid()}','${req.body.username}','${req.body.password}')`);
+        const access_code = uuid();
+        connection.execute(`insert into tbl_59_users(access_code,user_name,user_password) values('${access_code}','${req.body.username}','${req.body.password}')`);
 
         connection.end();
-        res.status(200).send('Successfuly inserted username and password.');
+        res.status(200).send(`Successfuly inserted username and password, your access code is: ${access_code} `);
     }
+
+
 
 
 
